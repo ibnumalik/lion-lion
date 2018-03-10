4642cfb16b831870cba21cb690cfa7fd401f31be
Shaiful Aiman Malik <ibnumalik@tbd.my>
2018-03-10 17:19:56 +0800

    Add footer to the website

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { BlogComponent } from './blog/blog.component';
 import { StoresComponent } from './stores/stores.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     BlogComponent,
-    StoresComponent
+    StoresComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
