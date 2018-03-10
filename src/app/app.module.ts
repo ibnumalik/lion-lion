554663cc099cabb71203e7184e8ffe6e407d924c
Shaiful Aiman Malik <ibnumalik@tbd.my>
2018-03-10 16:41:11 +0800

    Add stores section in index page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 
 import { AppComponent } from './app.component';
 import { BlogComponent } from './blog/blog.component';
+import { StoresComponent } from './stores/stores.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    BlogComponent
+    BlogComponent,
+    StoresComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/stores/stores.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+
+@Component({
+  selector: 'stores',
+  templateUrl: './stores.component.html',
+  styleUrls: ['./stores.component.scss']
+})
+export class StoresComponent{
+  stores: any[];
+  places: any[];
+
+  constructor(private http: Http) {
+    this.http.get('./assets/stores.json').subscribe(response => {
+      this.stores = response.json();
+      this.collectPlaces();
+    });
+  }
+
+  collectPlaces() {
+    console.log(this.stores);
+    this.places = Array.from(new Set(this.stores.map((store: any) => store.state)));
+  }
+
+}
