8c32d4d9b50f30026d6dd59ce30d2795ab0262ad
Shaiful Aiman Malik <ibnumalik@tbd.my>
2018-03-15 14:56:17 +0800

    Add ng-bootstrap dependencies to the website

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { BlogComponent } from './blog/blog.component';
@@ -27,7 +28,8 @@ import { SortByPipe } from './sort-by.pipe';
   imports: [
     BrowserModule,
     HttpModule,
-    FormsModule
+    FormsModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
